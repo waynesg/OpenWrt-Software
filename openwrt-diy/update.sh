49ca306d0be286f57c7c64759441f640b974a060
Wayne Zhong <82129072+waynesg@users.noreply.github.com>
2022-04-21 23:32:03 +0800

    Update update.sh

--- openwrt-diy/update.sh
+++ openwrt-diy/update.sh
@@ -26,8 +26,8 @@ svn co https://github.com/QiuSimons/openwrt-mos/trunk ./luci-app-mosdns
 svn co https://github.com/jerrykuku/luci-app-vssr/trunk ./luci-app-vssr
 
 #Bypass
-svn co https://github.com/kiddin9/openwrt-bypass/trunk ./luci-app-bypass
-svn co https://github.com/kiddin9/openwrt-packages/trunk/tcping ./luci-app-bypass/tcping
+svn co https://github.com/kiddin9/openwrt-bypass/trunk/luci-app-bypass ./luci-app-bypass
+#svn co https://github.com/kiddin9/openwrt-packages/trunk/tcping ./luci-app-bypass/tcping
 #SSR-PLUS
 svn co https://github.com/fw876/helloworld/trunk ./luci-app-ssr-plus
 #svn co https://github.com/fw876/helloworld/trunk/xray-core ./xray-core
